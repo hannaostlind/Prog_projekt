7bca11fb3e67bbc3d5093f69d54e306c8aeb47a9
Felicia-Lager-Lofgren-Nti-Johanneberg <97357777+Felicia-Lager-Lofgren-Nti-Johanneberg@users.noreply.github.com>
2022-04-19 11:17:34 +0200

    hej
    Detta är vad vi gjorde 11:17 (:

--- NIM.rb
+++ NIM.rb
@@ -1,1 +1,31 @@
-hello world
+#NIM
+
+# PVP
+# 14 pinnar
+# plocka max 3 pinnar
+# MÅL = inte plocka sista pinnen
+# Steg 1 skriva in namn
+#slumpa antal pinnar och slumpa vilken spelare som börjar 
+
+
+# Beskrivning 
+
+
+def Nim()
+
+    puts "Hej och välkommen till spelet NIM"                   
+
+    puts "Spelare 1, skriv in ditt namn här"
+    input_spelare_1 = gets.chomp 
+
+    puts "Spelare 2, skriv in ditt namn här"
+    input_spelare_2 = gets.chomp 
+
+    puts "Detta är de antal pinnar ni kommer att spela med:"
+    antal_pinnar =  puts rand(0..14)
+
+    puts "Selare #{rand(1..2)} kommer att få börja"
+    
+end
+
+Nim()
